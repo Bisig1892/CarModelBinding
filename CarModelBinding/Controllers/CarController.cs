65a54931b6ed727cd32d301a7dd8e2cac9ca4722
Tyler <bisig1892@gmail.com>
2020-07-25 12:01:06 -0700

    Created Car controller with add route

---
+++ CarModelBinding/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarModelBinding.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarModelBinding.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Car c)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["Message"] = $"{c.Make} {c.Model} will start production on {c.Year}";
+                // Add to database here...
+            }
+            return View();
+        }
+    }
+}
