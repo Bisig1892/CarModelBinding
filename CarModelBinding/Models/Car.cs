632f7d23de54ba8d79a75b14c7159fcaa7e1ba07
Tyler <bisig1892@gmail.com>
2020-07-25 11:07:31 -0700

    Add project files.

---
+++ CarModelBinding/Models/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarModelBinding.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        /// <summary>
+        /// Make of the vehicle
+        /// </summary>
+        [Required]
+        public string Make { get; set; }
+        /// <summary>
+        /// Model of the Vehicle
+        /// </summary>
+        [Required]
+        public string Model { get; set; }
+        /// <summary>
+        /// Production year of the vehicle
+        /// </summary>
+        [DataType(DataType.Date]
+        [Required]
+        public DateTime Year { get; set; }
+        /// <summary>
+        /// Engine of the Vehicle 
+        /// </summary>
+        [Required]
+        public string Engine { get; set; }
+        /// <summary>
+        /// Interior and Exterior Trim for the Vehicle
+        /// </summary>
+        [Required]
+        public string Trim { get; set; }
+    }
+}
